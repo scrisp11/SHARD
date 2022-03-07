b02b81bce6d4c95d84f683c9b15c113270ebfc19
scrisp11 <75222722+scrisp11@users.noreply.github.com>
2022-03-07 13:35:23 -0800

    Update aperture.m
    Add phi_tap input to aperture to allow variable aperture

--- aperture.m
+++ aperture.m
@@ -1,9 +1,9 @@
-function focus_new = aperture(focus_old,y)
+function focus_new = aperture(focus_old,y,phi_tap)
 npart=size(y,2)/4;
 x = y(2*npart+1:npart*3);
 
 %% Aperture
-gapsize = 1000e-9;
+gapsize = phi_tap.aperture;
 focus_new = focus_old.*(abs(x(1,:))<gapsize/2);
 
 end
